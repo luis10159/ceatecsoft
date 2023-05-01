430b5f8996443df700cb719a7760faf16393aaf9
luis10159 <97810228+luis10159@users.noreply.github.com>
2023-05-01 01:09:54 -0500

    Add files via upload

--- src/modules/Login/layouts/MainLayout.vue
+++ src/modules/Login/layouts/MainLayout.vue
@@ -1,9 +1,17 @@
 <template>
+  
   <router-view></router-view>
 </template>
 <style lang="scss">
 .login {
-  background-color: #F9F9F9;
+
+
+
+background: linear-gradient(to right, #E2E2E2, #C9D6FF);
+
+
+// background: linear-gradient(to right, #1D2671, #701d37); 
+
 }
 .router-link-exact-active {
   color: #42b983;
